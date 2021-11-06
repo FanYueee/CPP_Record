4aaf453cb857fbca27f89b9790b3947772482efd
FanYueee <fanyueee@gmail.com>
2021-11-06 14:42:24 +0800

    20211106_Part1
    IO_Speedup

---
+++ IO_Speedup.cpp
@@ -0,0 +1,14 @@
+//IO Speedup
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	//==============================================================
+	//IO Speedup 加速;加後不可和printf()與scanf()混用;使用'\n'取代endl 
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	//==============================================================
+	
+	return 0;
+ } 
---
+++ ZeroJudge/a001.cpp
@@ -0,0 +1,14 @@
+//a001
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	string s1;
+	
+	cin >> s1;
+	
+	cout << "hello, " << s1 << endl;
+	
+	return 0;
+ } 
