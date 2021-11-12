487a22f6adb4ce0c5b2989b682eb805f4b924102
FanYueee <fanyueee@gmail.com>
2021-11-12 21:53:37 +0800

    20211112

---
+++ P1.cpp
@@ -0,0 +1,42 @@
+//Part1
+#include <iostream>
+using namespace std; //test
+
+int main(void)
+{
+	cout << "I am a student" << endl;
+	
+	cout << "Wellcome ";
+	cout << "Dev C++ 5.11\n";
+	
+	cout << "Welcome\nDev\nC++\n5.11\n";
+	
+	cout << "012345678901234567890123456789\n";
+	
+	cout << "\tOutput Tab\n";
+	cout << "\t\tOutput Tab\n";
+	cout << "\t\t\tOutput Tab\n";	
+	
+	int i;
+	char c;
+	bool b;
+	long long l;
+	
+	int integer1;
+	
+	cout << "?" << '\n'
+	cin >> integer1;
+	cout << "===" << integer1 << endl;
+	
+	
+	int cin1;
+	int cin2, cin3;
+	
+	cin >> cin1;
+	cin >> cin2;
+	
+	cin3 = cin1 + cin2
+	cout << "Cin3 is " << 
+	return 0;
+}
+
--- Part1.cpp
+++ Part1.cpp
@@ -1,42 +1,28 @@
-//Part1
+/*
+這是多行註解，可以很多行
+*/
+//多行註解範圍就是 /* 到 */ 的範圍
+
+//這是單行註解，//為開始到該行結束
+
+/*
+#include <iostream> 前置處理器指示，標頭檔，就類似給予電腦一本字典一樣，要讓電腦理解接下來的程式。
+*/
 #include <iostream>
-using namespace std; //test
 
-int main(void)
+/*
+using namespace std; 意思為告訴電腦這個程式的所有東西(using namespace)都在同個地方(std)標準函式庫內。
+
+如果沒有的話每次使用標準函式庫的函數跟符號常數時，前面都必須加 std:: ，那不是很麻煩。
+*/
+using namespace std;
+
+/*
+主程式開始!
+int 代表回傳的型態都是整數
+main 函數名稱，所有程式都要有個主程式做為整個開始，而 main(主要) 就代表主程式了喔!
+*/
+int  main(void)
 {
-	cout << "I am a student" << endl;
-	
-	cout << "Wellcome ";
-	cout << "Dev C++ 5.11\n";
-	
-	cout << "Welcome\nDev\nC++\n5.11\n";
-	
-	cout << "012345678901234567890123456789\n";
-	
-	cout << "\tOutput Tab\n";
-	cout << "\t\tOutput Tab\n";
-	cout << "\t\t\tOutput Tab\n";	
-	
-	int i;
-	char c;
-	bool b;
-	long long l;
-	
-	int integer1;
-	
-	cout << "?" << '\n'
-	cin >> integer1;
-	cout << "===" << integer1 << endl;
-	
-	
-	int cin1;
-	int cin2, cin3;
-	
-	cin >> cin1;
-	cin >> cin2;
-	
-	cin3 = cin1 + cin2
-	cout << "Cin3 is " << 
-	return 0;
-}
 
+}
